69290e41431db8d9e7e18e6a9228e0078214f102
rksm2329 <113524713+Codehyx@users.noreply.github.com>
2023-10-16 13:41:41 +0800

    A+B Problem.py

---
+++ A+B Problem.py
@@ -0,0 +1,29 @@
+print("输入一个形如 a+b 的算式:")
+s = str(input())
+cnt = 0
+a = [0, 0]
+op = ''
+for i in range(0, len(s)) :
+    if s[i].isdigit() :
+        a[cnt] = a[cnt] * 10 + int(s[i])
+    else :
+        op = s[i]
+        cnt = cnt + 1;
+if op == '+' :
+    print(a[0] + a[1])
+elif op == '-' :
+    print(a[0] - a[1])
+elif op == '*' :
+    print(a[0] * a[1])
+elif op == '/' :
+    print(a[0] / a[1])
+elif op == '%' :
+    print(a[0] % a[1])
+elif op == '&' :
+    print(a[0] & a[1])
+elif op == '|' :
+    print(a[0] | a[1])
+elif op == '^' :
+    print(a[0] ^ a[1])
+else :
+    print("计算失败！！")
