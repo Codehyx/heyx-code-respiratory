9868234aa9b0b05b7894e9aec6d85cfc25070d6d
rksm2329 <113524713+Codehyx@users.noreply.github.com>
2023-10-17 21:36:32 +0800

    Logical Expression Calculator

---
+++ Logical_Expression_Calculator.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+string s;
+bool ans;
+int status;
+
+int main() {
+  cin >> s;
+  for (int i = 0; i < s.size(); i++) {
+    if (status) {
+      if (s[i] == '(') {
+        int x = 1;
+        while (x) {
+          i++;
+          if (s[i] == '(') {
+            x++;
+          }
+          if (s[i] == ')') {
+            x--;
+          }
+        }
+      } else if (status == 1 && s[i] == '|' || s[i] == ')') {
+        status = 0;
+      }
+    } else {
+      if (s[i] == '1') {
+        ans = 1;
+      }
+      if (s[i] == '0') {
+        ans = 0;
+      }
+      if (s[i] == '&' && ans == 0) {
+        status = 1;
+      }
+      if (s[i] == '|' && ans == 1) {
+        status = 2;
+      }
+    }
+  }
+  cout << ans;
+  return 0;
+}
