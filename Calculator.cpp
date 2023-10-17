8f8df59f9cc196a371fce3b05d80c7ba1940351d
rksm2329 <113524713+Codehyx@users.noreply.github.com>
2023-10-17 23:29:03 +0800

    Scientific Computer

--- Calculator.cpp
+++ Calculator.cpp
@@ -343,8 +343,7 @@ int cal_factorial_number(string &theStr) {
 }
 
 int cal_factorial(int theInt) {
-  LargeNumberFactorial[0] = 1;
-  LargeNumberFactorial[1] = 1;
+  LargeNumberFactorial[0] = LargeNumberFactorial[1] = 1;
   int digit = 1;
   for (int i = 1; i <= theInt; i++) {
     for (int j = 1; j <= digit; j++) {
